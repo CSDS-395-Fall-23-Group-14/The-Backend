72070ba4f22034b982a5f9035347fab427fb49a6
cgw43 <98435377+cgw43@users.noreply.github.com>
2023-10-05 10:21:51 -0400

    Cleaning Data

--- OptionsScraper.js
+++ OptionsScraper.js
@@ -18,7 +18,7 @@ const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, mil
 
 
 async function optionsScraper() {
-    const browser = await puppeteer.launch( {headless:false} );
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('https://finance.yahoo.com/options/highest-open-interest/?count=100&offset=0');
 
@@ -26,70 +26,70 @@ async function optionsScraper() {
     let stonks = [];
     while (flag) {
 
-    
-    const data = await page.evaluate(async function () {
 
-        const today = new Date();
-        const dateTaken = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
+        const data = await page.evaluate(async function () {
 
-        const tbody = document.querySelector("tbody");
+            const today = new Date();
+            const dateTaken = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 
-        const rows = Array.from(tbody.querySelectorAll("tr"));
-        let tempStonks = [];
-        for (i=0;i<rows.length;i++) {
-            const tds = Array.from(rows[i].querySelectorAll("td"));
-            const cols = tds.map((td) => td.innerText);
-            tempStonks.push(
-                {
-                    linkthing: cols[0],
-                    symbol: cols[1],
-                    name: cols[2],
-                    position: cols[2].slice(-4).replace(" ", ""),
-                    strike: cols[3],
-                    expiration: cols[4],
-                    intradayprice: cols[5],
-                    change: cols[6],
-                    percentchange: cols[7],
-                    bid: cols[8],
-                    ask: cols[9],
-                    volume: cols[10],
-                    openinterest: cols[11],
-                    dateScraped: dateTaken
-                }
-            )
+            const tbody = document.querySelector("tbody");
+
+            const rows = Array.from(tbody.querySelectorAll("tr"));
+            let tempStonks = [];
+            for (i = 0; i < rows.length; i++) {
+                const tds = Array.from(rows[i].querySelectorAll("td"));
+                const cols = tds.map((td) => td.innerText);
+                tempStonks.push(
+                    {
+                        optionid: cols[0],
+                        ticker: cols[1],
+                        name: cols[2],
+                        position: cols[2].slice(-4).replace(" ", ""),
+                        strike: +(cols[3].replaceAll(",", "")),
+                        expiration: cols[4],
+                        intradayprice: +(cols[5].replaceAll(",", "")),
+                        change: +(cols[6].replace("+", "").replaceAll(",", "")),
+                        percentchange: +(cols[7].replace("+", "").replaceAll(",", "").replace("%", "")),
+                        bid: +(cols[8].replaceAll(",", "")),
+                        ask: +(cols[9].replaceAll(",", "")),
+                        volume: +(cols[10]).replaceAll(",", ""),
+                        openinterest: +(cols[11].replaceAll(",", "")),
+                        dateScraped: dateTaken
+                    }
+                )
+            }
+
+            return tempStonks;
+        });
+
+        // Check for last page
+        if (data.length != 100) {
+            flag = false;
         }
-        
-        return tempStonks;
-    });
 
-     // Check for last page
-    if (data.length != 100) {
-        flag = false;
-    }
-
-    stonks = [...data, ...stonks];
+        stonks = [...data, ...stonks];
 
 
 
-    const [next] = await page.$x('//*[@id="scr-res-table"]/div[2]/button[3]');
+        const [next] = await page.$x('//*[@id="scr-res-table"]/div[2]/button[3]');
 
-    await next.click();
-    await delay(750);
+        await next.click();
+        await delay(750);
     }
     browser.close();
-    
+
     return stonks;
 }
 
 async function optionsPart2() {
-    const browser = await puppeteer.launch( {headless:false} );
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
     const url = 'https://finance.yahoo.com/options/highest-open-interest/?count=100&offset=';
     var offset = 0;
     var flag = true;
     let stocks = [];
-    
+
     while (flag) {
         await page.goto(url + offset);
 
@@ -99,28 +99,28 @@ async function optionsPart2() {
             const dateTaken = (today.getMonth() + 1) + "/" + today.getDate() + "/" + today.getFullYear();
 
             const tbody = document.querySelector("tbody");
-    
+
             const rows = Array.from(tbody.querySelectorAll("tr"));
             let tempStonks = [];
-            for (i=0;i<rows.length;i++) {
+            for (i = 0; i < rows.length; i++) {
                 const tds = Array.from(rows[i].querySelectorAll("td"));
                 const cols = tds.map((td) => td.innerText);
                 //position - put or call from name
                 tempStonks.push(
                     {
-                        linkthing: cols[0],
-                        symbol: cols[1],
+                        optionid: cols[0],
+                        ticker: cols[1],
                         name: cols[2],
                         position: cols[2].slice(-4).replace(" ", ""),
-                        strike: cols[3],
+                        strike: +(cols[3].replaceAll(",", "")),
                         expiration: cols[4],
-                        intradayprice: cols[5],
-                        change: cols[6],
-                        percentchange: cols[7],
-                        bid: cols[8],
-                        ask: cols[9],
-                        volume: cols[10],
-                        openinterest: cols[11],
+                        intradayprice: +(cols[5].replaceAll(",", "")),
+                        change: +(cols[6].replace("+", "").replaceAll(",", "")),
+                        percentchange: +(cols[7].replace("+", "").replaceAll(",", "").replace("%", "")),
+                        bid: +(cols[8].replaceAll(",", "")),
+                        ask: +(cols[9].replaceAll(",", "")),
+                        volume: +(cols[10]).replaceAll(",", ""),
+                        openinterest: +(cols[11].replaceAll(",", "")),
                         dateScraped: dateTaken
                     }
                 )
@@ -128,29 +128,29 @@ async function optionsPart2() {
             console.log(tempStonks[0])
             return tempStonks;
         });
-    
-         // Check for last page
+
+        // Check for last page
         if (data.length != 100) {
             flag = false;
         }
         offset += 100;
         console.log(data[1]);
         stocks = [...data, ...stocks];
-        
-       await delay(2304);
+
+        await delay(2304);
     }
 
-   // console.log("Closing");
+    // console.log("Closing");
     await delay(100);
 
     await browser.close();
     return stocks;
-    
+
 }
 
 async function printSomeOptions() {
     const options = await optionsScraper();
-    
+
     let count = 0;
     options.forEach(opt => {
         if (count == 0) {
@@ -165,17 +165,17 @@ async function printSomeOptions() {
 
 }
 
-async function filterStocks () {
+async function filterStocks() {
 
     const russell = await r2k.betterSite();
     const options = await optionsPart2();
 
     console.log("Thinking...");
 
-   const res = options.filter(opt => {
-    let temp = russell.filter(russ => russ.ticker == opt.symbol);
-    return temp.length > 0;
-    } ) 
+    const res = options.filter(opt => {
+        let temp = russell.filter(russ => russ.ticker == opt.ticker);
+        return temp.length > 0;
+    })
 
     console.log("Getting results...")
 
@@ -189,9 +189,9 @@ async function writeOptionsToFile() {
     const options = await optionsPart2();
 
     const res = options.filter(opt => {
-        let temp = russell.filter(russ => russ.ticker == opt.symbol);
+        let temp = russell.filter(russ => russ.ticker == opt.ticker);
         return temp.length > 0;
-    } );
+    });
     const content = JSON.stringify(res);
 
     fs.writeFile("./data/OptionsRussell2K.json", content, err => {
